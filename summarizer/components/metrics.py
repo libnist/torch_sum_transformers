da539270e45415aea9e115ec22b4cf250ea1f54e
unknown <ghahremani.mim@gmail.com>
2022-12-17 17:22:39 +0330

    add avging to nll loss

--- summarizer/components/metrics.py
+++ summarizer/components/metrics.py
@@ -9,9 +9,10 @@ nllloss = nn.NLLLoss()
 
 def nll_loss(y_pred: torch.tensor,
              y_true: torch.tensor):
+    
     loss = nllloss(torch.log(torch.softmax(y_pred[0], dim=-1)),
                    y_true[0])
     for i in range(1, y_pred.shape[0]):
         loss += nllloss(torch.log(torch.softmax(y_pred[i], dim=-1)),
                         y_true[i])
-    return loss
+    return loss/y_pred.shape[0]
