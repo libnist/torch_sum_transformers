b7e2137c2b31cd0afd9c6dd81826d0e24f25fa7e
unknown <ghahremani.mim@gmail.com>
2022-12-17 12:46:20 +0330

    none reduction

--- transformers/components/metrics.py
+++ transformers/components/metrics.py
@@ -3,11 +3,12 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-cross_entropy = nn.CrossEntropyLoss()
+cross_entropy = nn.CrossEntropyLoss(reduction="none")
 
 # Cross entropy loss
 def cross_entropy_loss(y_pred: torch.tensor,
                        y_true: torch.tensor,
                        vocab_size: int):
     categorical_y_true = F.one_hot(y_true, vocab_size).float()
-    return cross_entropy(y_pred, categorical_y_true)
+    loss =  cross_entropy(y_pred, categorical_y_true)
+    loss = (loss.sum(dim=-1)/63).sum()
